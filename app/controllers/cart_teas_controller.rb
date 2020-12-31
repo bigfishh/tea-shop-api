6f458b077fe6e596b42d3dcc8ed99d6b91767b17
bigfishh <anniezheng.anniee@gmail.com>
2020-12-30 23:57:44 -0500

    updates decrease tea quantity logic

--- app/controllers/cart_teas_controller.rb
+++ app/controllers/cart_teas_controller.rb
@@ -1,11 +1,18 @@
 class CartTeasController < ApplicationController
     
     def create
+        user = logged_in_user
         found_cart = Cart.find(params[:cart_id])
         if found_cart.cart_teas.pluck(:tea_id).include?(params[:tea_id])
             found_cart_tea = found_cart.cart_teas.find_by(tea_id: params[:tea_id])
-            found_cart_tea.update(quantity: params[:quantity])
-            render json: found_cart_tea
+            if params[:quantity] <= 0 
+                found_cart_tea_id = found_cart_tea.id
+                found_cart_tea.destroy
+                render json: {cart: user.current_cart, deleted_item_id: found_cart_tea_id}
+            else
+                found_cart_tea.update(quantity: params[:quantity])
+                render json: found_cart_tea
+            end
         else 
             new_cart_tea = CartTea.create(cart_tea_params)
             render json: new_cart_tea
@@ -13,11 +20,10 @@ class CartTeasController < ApplicationController
     end
 
     def destroy 
-        user = logged_in_user
         found_tea = CartTea.find(params[:id])
         found_tea.delete
 
-        render json: {cart: user.cart, removed_cart_tea_id: found_tea.id}
+        render json: {cart: user.current_cart}
     end
 
     private 
